bec77873db9badfc50509cd95a15797d9d12d735
Luke Branch <lukebranch@gmail.com>
2015-01-03 16:29:09 +0800

    Create __openerp__.py

---
+++ __openerp__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Sale Category List View Extended',
+    'description': 'This module adds extra fields to the category list view for product categories and public product categories',
+    'category': 'Website',
+    'version': '1.0',
+    'author': 'Luke Branch',
+    'depends': ['product','sale'],
+    'data': [
+        'views/category_list_view.xml',
+    ],
+    'application': True,
+}
